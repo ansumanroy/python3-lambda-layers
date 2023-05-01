2067c4f309cce1ff99facf17d15943a99484d4b8
ansuman.roy@cevo.com.au <ansuman.roy@cevo.com.au>
2023-05-01 16:54:54 +1000

    adding lambda

--- lambda/build.sh
+++ lambda/build.sh
@@ -7,7 +7,7 @@ mkdir -p packages/
 
 # Building Python-pandas layer
 cd ${WRKDIR}/${LYR_PDS_DIR}/
-${WRKDIR}/${LYR_PDS_DIR}/build_layer.sh
-zip -r ${WRKDIR}/packages/Python3-pandas.zip .
+${WRKDIR}/${LYR_PDS_DIR}/build_layer2.sh
+zip -r ${WRKDIR}/packages/Python3-pandas.zip . -x ".git/*"
 rm -rf ${WRKDIR}/${LYR_PDS_DIR}/python/
 
